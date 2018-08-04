905610fcd919ca42a26ecb8e7062922975f502db
lothbrokragnar <gcuevas@sodimac.cl>
2018-08-04 13:35:28 -0400

    first commit

---
+++ controllers/main.js
@@ -0,0 +1,13 @@
+exports.home = (req,res) => {
+    res.send("Bienvenido a Mi Pagina");
+    res.end();
+};
+
+exports.contacto = (req,res) => {
+    res.send("Puedes contactarme al correo me@example.com");
+    res.end();
+};
+
+
+
+
---
+++ settings/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    SERVER:{
+        port: 5000
+    }
+}
+
